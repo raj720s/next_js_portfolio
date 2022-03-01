72090ad4e6a5afef569af711a3acc9caa6b61a06
raj720s <raj720s@raj.com>
2022-03-01 17:04:48 +0530

    for fun

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! with kd</a>
+          Welcome to <a href="https://nextjs.org">Next.js! with raj kd</a>
         </h1>
       </main>
 
